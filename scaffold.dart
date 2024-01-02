8d7b433cdb8fcd232922eb3646fe81ca64b1d69e
natansh05 <202201445@daiict.ac.in>
2024-01-02 13:33:23 +0530

    drawer ka code add karna baki hai

---
+++ scaffold.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Scaffold Trial'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),//App Bar widget
+      backgroundColor: Colors.lightBlue[200],
+      body: const Center(
+        child: Text('Scaffold Widget Example'),
+      ),// main text which appears in middle by default
+      floatingActionButton: FloatingActionButton(
+        child: const Text('T'),
+        onPressed: () {},
+      ),//floating action button appears on bottom right by default
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_alarms_rounded),
+            //icon  widget and property
+            label: 'Access Alarms',
+          )//Bottom navigation bar 
+        ],//items added to dispaly in bottom navigation bat
+      ),//Bottom navigation bar 
+    ),
+  ));
+  
+}
